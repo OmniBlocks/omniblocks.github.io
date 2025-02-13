b191902cd686a498353e2f5beea79a0c15f84d12
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-13 19:43:02 +0000

    Update build-ultiblocks.sh

--- build-ultiblocks.sh
+++ build-ultiblocks.sh
@@ -1,10 +1,4 @@
-echo "::group::Cloning repository"
 git clone https://github.com/UltiBlocks/scratch-gui --depth=1
-echo "::endgroup::"
-echo "::group::Install scratch-gui"
 cd scratch-gui
 npm ci
-echo "::endgroup::"
-echo "::group::Build scratch-gui"
 npm run build
-echo "::endgroup::"
