5e19307c5f8b1e3a0f499e688c8dfc667c1ad4de
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-16 21:29:49 +0000

    Use old script again (other deployment wasn't showing block correctly

--- build-ultiblocks.sh
+++ build-ultiblocks.sh
@@ -1,4 +1,22 @@
+echo "::group::Cloning repositories"
 git clone https://github.com/UltiBlocks/scratch-gui --depth=1
+git clone https://github.com/UltiBlocks/scratch-vm --depth=1
+git clone https://github.com/UltiBlocks/scratch-blocks --depth=1
+echo "::endgroup::"
+echo "::group::Build scratch-blocks"
+cd scratch-blocks
+npm ci
+cd ..
+echo "::endgroup::"
+echo "::group::Build scratch-vm"
+cd scratch-vm
+npm ci
+cd ..
+echo "::endgroup::"
+echo "::group::Build scratch-gui"
 cd scratch-gui
 npm ci
+npm link ../scratch-vm
+npm link ../scratch-blocks
 npm run build
+echo "::endgroup::"
