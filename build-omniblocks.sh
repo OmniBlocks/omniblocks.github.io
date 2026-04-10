6d73a3827c4c9d7708a59064834a8070c89402fc
AmpElectrecuted <266199358+ampelc@users.noreply.github.com>
2026-04-10 17:24:41 +0100

    Update build-omniblocks.sh

--- build-omniblocks.sh
+++ build-omniblocks.sh
@@ -1,3 +1,5 @@
+set -euo pipefail
+
 echo "::group::Cloning repository"
 git clone https://github.com/OmniBlocks/monorepo --depth=1
 cd monorepo
@@ -6,7 +8,5 @@ echo "::endgroup::"
 echo "::group::Building"
 pnpm i --shamefully-hoist
 cd scratch-gui
-echo "🚀 Building with APP_VERSION=$APP_VERSION"
-# Explicitly pass APP_VERSION to npm
 ROOT=/ NODE_ENV=production pnpm build
 echo "::endgroup::"
