b027931db7231238db09cf63fdecbc760b528a3c
playforge-coding <oliverjohnkim@icloud.com>
2026-07-23 17:45:12 -0500

    Enhance build script with dependency installation and builds

--- build-omniblocks.sh
+++ build-omniblocks.sh
@@ -1,12 +1,28 @@
 set -euo pipefail
-
+ 
 echo "::group::Cloning repository"
 git clone https://github.com/OmniBlocks/monorepo --depth=1
 cd monorepo
 echo "::endgroup::"
-
+ 
 echo "::group::Building"
+echo "::group::Installing dependencies"
 pnpm i --shamefully-hoist
+echo "::endgroup::"
+
+echo "::group::Building editor"
 cd scratch-gui
 ROOT=/ NODE_ENV=production pnpm build
+cd ..
+echo "::endgroup::"
+
+echo "::group::Building extensions gallery"
+cd extensions
+pnpm build
+cd ..
+echo "::endgroup::"
+
+echo "::group::Adding gallery to Pages artifact"
+mkdir -p scratch-gui/build/extensions
+cp -a extensions/build/. scratch-gui/build/extensions/
 echo "::endgroup::"
